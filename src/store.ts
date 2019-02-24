16ad280e7465827dd85d146bae2aaddfc2f3e080
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-02-24 15:58:33 +0100

    Load setup.yml to Vuex store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+const config = require("./config/setup.yml");
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    ...config
+  },
   mutations: {},
   actions: {}
 });
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule("yml")
+      .test(/\.yml$/)
+      .use("json-loader")
+      .loader("json-loader")
+      .end()
+      .use("yaml-loader")
+      .loader("yaml-loader");
+  }
+};
