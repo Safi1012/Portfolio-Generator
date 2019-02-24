9157589992c5caadd8cc81ce147cf62282564db8
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-02-24 21:13:14 +0100

    Create ThemeProvider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ThemeProvider />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+  import { Component, Vue } from 'vue-property-decorator';
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import ThemeProvider from '@/components/ThemeProvider.vue';
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    ThemeProvider
   }
 })
-export default class Home extends Vue {}
+  export default class Home extends Vue {}
 </script>
