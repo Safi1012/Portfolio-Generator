4c9df5ef6a471be3ad73231845a390d372b8b22c
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-05-03 22:44:37 +0200

    Remove unneeded 'vue-svgicon' dependency

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import Vue from 'vue';
-import SvgIcon from 'vue-svgicon';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
-
-Vue.use(SvgIcon, {
-  tagName: 'svg-icon',
-  defaultWidth: '2rem',
-  defaultHeight: '2rem'
-});
 
 new Vue({
   router,
