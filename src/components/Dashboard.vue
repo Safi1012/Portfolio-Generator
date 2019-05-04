5fb986eda123d891aaac1033b43a0ed60567bb09
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-05-04 15:28:59 +0200

    Scaffold basic Dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Hello Dashboard11</h1>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator'
+  import { mapState } from 'vuex'
+
+  interface Project {
+    name: string;
+    url: string;
+    icon: string;
+    backgroundColor: string;
+  }
+
+  @Component({
+    computed: mapState([
+      'portfolio',
+      'projects',
+    ]),
+  })
+  export default class Dashboard extends Vue {
+    public portfolio!: string;
+    public projects!: Project[];
+
+    mounted() {
+      console.log(this.portfolio);
+      console.log(this.projects);
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  h1 {
+    color: var(--primary-color);
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Header />
+    <Dashboard />
     <ProjectCard />
   </div>
 </template>
@@ -10,10 +11,12 @@
   import { Component, Vue } from 'vue-property-decorator';
   import Header from '@/components/Header.vue';
   import ProjectCard from '@/components/ProjectCard.vue';
+  import Dashboard from '@/components/Dashboard.vue';
 
   @Component({
     components: {
       Header,
+      Dashboard,
       ProjectCard
     }
   })
