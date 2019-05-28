6fe5bb07fe03cdee8bb59bc8c830ecd458a53600
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-05-28 20:24:40 +0200

    Adjust header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,10 @@
 <template>
   <ul>
-    <li>
-      <router-link to="/">
-        <img src="@/assets/icons/showroom.svg" alt="showroom icon" />
-      </router-link>
+    <li v-for="(social, index) in socialMedia" :key="index">
+      <a :href="social['url']" target="_blank" rel="noopener nofollow">
+        <div class="icon-mask" :style="{ 'mask-image': getCSSImageURL(social['name']) }" />
+      </a>
     </li>
-
-    <div>
-      <li v-for="(social, index) in socialMedia" :key="index">
-        <a :href="social['url']" target="_blank" rel="noopener nofollow">
-          <div class="icon-mask" :style="{ 'mask-image': getCSSImageURL(social['name']) }" />
-        </a>
-      </li>
-    </div>
   </ul>
 </template>
 
@@ -44,7 +36,7 @@
   ul {
     display: flex;
     position: fixed;
-    justify-content: space-between;
+    justify-content: flex-end;
     width: 100vw;
     background-color: var(--secondary-color);
     margin: 0;
@@ -56,6 +48,10 @@
     list-style: none;
     display: inline;
     margin: 1rem 0.5rem;
+  }
+
+  li:last-child {
+    margin-right: 1.5rem;
   }
 
   div {
@@ -71,16 +67,47 @@
     stroke: none;
   }
 
-  img {
-    width: 2rem;
-    height: 2rem;
-  }
-
   .icon-mask {
     background-color: var(--primary-color);
-    width: 2rem;
-    height: 2rem;
+    width: 1.5rem;
+    height: 1.5rem;
     mask-repeat: no-repeat;
     mask-position: center;
+  }
+
+  @media (min-width: 768px) {
+    li {
+      margin: 1rem 0.65rem;
+    }
+
+    .icon-mask {
+      width: 1.65rem;
+      height: 1.65rem;
+    }
+  }
+
+  @media (min-width: 768px) {
+    li {
+      margin: 1rem 0.65rem;
+    }
+
+    li:last-child {
+      margin-right: 1.8rem;
+    }
+
+    .icon-mask {
+      width: 1.65rem;
+      height: 1.65rem;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    li {
+      margin: 1.2rem 0.8rem;
+    }
+
+    li:last-child {
+      margin-right: 2rem;
+    }
   }
 </style>
