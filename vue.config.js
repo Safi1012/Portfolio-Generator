eb3d1075dbefc93efbf75fb0734d7276a712ca19
Filipe Santos Correa <filipe.santoscorrea@gmail.com>
2019-07-02 22:22:59 +0200

    Setup pre rendering

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const JSDOMRenderer = require('@prerenderer/renderer-jsdom');
+
 module.exports = {
   chainWebpack: config => {
     config.module
@@ -10,6 +14,20 @@ module.exports = {
       .loader('yaml-loader');
   },
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'), // The path to the webpack-outputted app to prerender.
+        routes: ['/'], // List of routes to prerender.
+        renderer: new JSDOMRenderer(),
+        minify: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          decodeEntities: true,
+          keepClosingSlash: true,
+          sortAttributes: true
+        }
+      })
+    ]
   }
 };
